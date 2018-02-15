57e94d14496dae10f6caef6a020824094d1fc150
sapiniol <sapiniol@users.noreply.github.com>
2018-02-15 21:28:19 +0100

    Add files via upload

--- moja_strona_projekt/js/functions.js
+++ moja_strona_projekt/js/functions.js
@@ -2,8 +2,16 @@ $(window).scroll(function(){
 
   let scroll = $(this).scrollTop();
 
-  $('#menu').css({
-    'transform' : 'translate(0px, '+scroll /2+'%)'
+  $('.Dane').css({
+    'transform' : 'translate(0px, -'+scroll *0.5+'%)'
+  });
+
+  $('.CV').css({
+    'transform' : 'translate(0px, -'+scroll /2+'%)'
+  });
+
+  $('.Kontakt').css({
+    'transform' : 'translate(0px, -'+scroll /2+'%)'
   });
 
 })
